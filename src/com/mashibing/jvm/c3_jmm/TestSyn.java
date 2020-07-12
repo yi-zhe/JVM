6b536168e2170d229d2e9bf528934a79ccd60ac0
liuyang <liuyang@coohua.com>
2020-07-12 11:09:40 +0800

    第四节-1

---
+++ src/com/mashibing/jvm/c3_jmm/TestSyn.java
@@ -0,0 +1,15 @@
+package com.mashibing.jvm.c3_jmm;
+
+public class TestSyn {
+    synchronized void m() {
+    }
+
+    void n() {
+        synchronized (this) {
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
